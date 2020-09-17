a224f8599c12c3f21c947479ab6599191544a12d
Hilman Azfar <m.hilman.azfar@gmail.com>
2020-09-17 16:24:39 +0800

    created single listing page layout

--- client/src/Pages/SingeListingPage/index.jsx
+++ client/src/Pages/SingeListingPage/index.jsx
@@ -1,22 +1,34 @@
 import React, { Component } from 'react'
+import './index.css'
+
 import { Col, Row } from 'react-bootstrap'
 
 export default class SingleListingPage extends Component {
   render() {
 
     // layout the page
-    //
+    // get dynamic data for single page
     return (
       <div>
         <Row>
-          Lender details
+          <div className="border p-Icon">Icon here</div>
+          <Col className="text-left">
+            <h4>Lender name</h4>
+            <p>more options?</p>
+          </Col>
         </Row>
         <Row>
           <Col>
             Image here
           </Col>
           <Col>
-            Item details here
+            <h3>item name</h3>
+            <p>detail 1</p>
+            <p>detail 2</p>
+            <p>detail 3</p>
+            <p>detail 4</p>
+            <p>detail 5</p>
+            <button>Request</button>
           </Col>
         </Row>
         <Row>
